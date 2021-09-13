9f51caad5dbd6ba36368a89aa780a778d3e6ced5
Your Name <827511@mystma.org>
2021-09-13 15:07:47 -0500

    Added top 2 movies

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello world!")
+print("do you want to play a game?")
+
+#Print first movie
+print("Raiders of the lost Ark")
+
+#print second movie
+print("Once upon a time in Hollywood")
+
+#print third movie
+print("Once upon a time in the west")
+
