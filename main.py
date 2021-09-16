2186f7ae894fac3f299c88877d46351532190b81
Your Name <827511@mystma.org>
2021-09-16 15:19:58 -0500

    mad lib

--- main.py
+++ main.py
@@ -75,3 +75,29 @@ def myNameIs(Dog):
 myNameIs("Gabriel Trotter")
 myNameIs("Go")
 
+def timesTen(num):
+    print(num * 10)
+
+timesTen(3)
+
+def plusFive(num):
+    print(num + 5)
+
+plusFive(3)
+
+def dividedByTwo(num):
+    print(num / 2)
+
+dividedByTwo(4)
+
+def addTwoNumbas(x,y):
+    print(x + y)
+
+addTwoNumbas(3,27)
+
+def madLib(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):
+    print("While I've stayed home, I've practaced" + a + "for" + b + "hours every" + c + "My favorite key signature to play and practice in is" + d + e + "It has" + f + "flats and" + g + "sharps. I like this key signature because it is" + h + "and" + i + "I've also gotten awesome at counting time signatures. The time signature I love is" + j + "because, it is" + k + "to count. One intrument I want to learn how to play while I'm home is" + l + "because it is" + m + "and sounds like a" + n + "My" + o + "likes it when I play" + p + "and always gives me a round of applause after my performances! When I return to school, my teacher will be" + q + "of how great I am at playing my instument. My teacher might even think I sound like" + r)
+
+madLib("guitar", 6, "tuesday", "E", "minor", 3, 4, "cool", "awesome", 1/4, "fun", "drums", "dope", "plane", "Dad", "DOA", "Excited", "EVH")
+
+
