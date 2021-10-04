54c5d7f23b0605eb6b78ccc71b5be5531ed9dc76
Your Name <827511@mystma.org>
2021-10-04 15:02:14 -0500

    notes

--- main.py
+++ main.py
@@ -1,103 +1,54 @@
-def topFiveMovies():
-    #Print first movie
-    print("Raiders of the Lost Ark")
+def count():
 
-    #Print second movie
-    print("Once Upon A Time In Hollywood")
+    x = 1
 
-    #print third movie
-    print("Spider-man 2")
+    while x < 5:
+        print("Turkey")
+        print("Ratchelor")
+        x = x + 1
 
-    #print fourth movie
-    print("Once Upon a Time in the West")
 
-    #print fifth movie
-    print("There Will Be Blood")
+count()
 
-topFiveMovies()
+def countTwo():
 
-def topFourBands():
-    #print first band
-    print("Van Halen")
+    x = 1
 
-    #Print second band
-    print("Hippo Campus")
+    while x <= 10:
+        x = x + 1
+        if x % 2 == 0:
+            print(x)
+            print("is an even number")
+        else:
+            print(x)
+            print("is an odd number")
+        
 
-    #print third band
-    print("The Clash")
+        
+countTwo() 
+
+myWord = "bulgaria"
+
+print(myWord[0])
+
+def firstLetter(w):
+    return w[5]
+
+print(firstLetter("no more fortnite"))
+
+
+def lastLetter(w):
+    return w[len(w) - 1]
+
+print(lastLetter("Hippo Campus"))
+
+
+def spellingBee(w):
     
-    #print fourth band
-    print("Weezer")
-
-topFourBands()
-
-def topThreeSports():
+    x = 0
     
-    #print first sport
-    print("Karate")
-
-    #Print second sport
-    print("Boxing")
-
-    #print third sport
-    print("Muay Thai")
-
-def topThreeVideoGames():
-
-    #Print first game
-    print("Assassin's Creed")
-
-    #Print second game
-    print("Elder Scrolls")
-
-    #Print third game
-    print("Uncharted")
-
-def topThreeFoods():
-    #print first food
-    print("Burgers") 
+    while x < len(w):
+        print(w[x])
+        x = x + 1
     
-    #print second food
-    print("Pizza")
-
-    #print third food
-    print("fruit salad")
-
-topThreeFoods()
-topThreeSports()
-topThreeVideoGames()
-
-print(27 + 2004)
-
-def myNameIs(Dog):
-    print(Dog + "Gabriel Trotter")
-
-myNameIs("Gabriel Trotter")
-myNameIs("Go")
-
-def timesTen(num):
-    print(num * 10)
-
-timesTen(3)
-
-def plusFive(num):
-    print(num + 5)
-
-plusFive(3)
-
-def dividedByTwo(num):
-    print(num / 2)
-
-dividedByTwo(4)
-
-def addTwoNumbas(x,y):
-    print(x + y)
-
-addTwoNumbas(3,27)
-
-def madLib(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):
-    print("While I've stayed home, I've practaced" + a + "for" + b + "hours every" + c + "My favorite key signature to play and practice in is" + d + e + "It has" + f + "flats and" + g + "sharps. I like this key signature because it is" + h + "and" + i + "I've also gotten awesome at counting time signatures. The time signature I love is" + j + "because, it is" + k + "to count. One intrument I want to learn how to play while I'm home is" + l + "because it is" + m + "and sounds like a" + n + "My" + o + "likes it when I play" + p + "and always gives me a round of applause after my performances! When I return to school, my teacher will be" + q + "of how great I am at playing my instument. My teacher might even think I sound like" + r)
-
-madLib("guitar", 6, "tuesday", "E", "minor", 3, 4, "cool", "awesome", 1/4, "fun", "drums", "dope", "plane", "Dad", "DOA", "Excited", "EVH")
-
-
+spellingBee("dogfood")
